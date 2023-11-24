b54bd7a246435b08a3cd644661fb410fb2731d1f
Dobeš Kristián (241497) <xdobes22@vutbr.cz>
2023-11-24 16:45:35 +0100

    feat: edit user

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = "users"
 
 urlpatterns = [
     path('<int:user_id>/', views.detail, name='detail'),
-    path('edit/', views.edit, name='edit'),
+    path('edit/<int:user_id>/', views.edit, name='edit'),
+    path('change_password/<int:user_id>/', views.change_password, name='change_password'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render, redirect
 from .models import Profile
-from .services import user_get_by_id
+from .services import user_get_by_id, verify_password
 
 def detail(request, user_id):
     user = user_get_by_id(request, user_id)
     return render(request, 'users/user_detail.html', {'profile': user})
 
-def edit(request):
+def edit(request, user_id):
     if request.method == 'POST':
         user = user_get_by_id(request, request.session['user_id'])
         user.username = request.POST['username']
+        user.profile_picture_url = request.POST['image_url']
+        user.bio = request.POST['bio']
         user.save()
-        return redirect('detail', user_id=user.id)
+        return redirect('users:detail', user_id=user.id)
     else:
-        return render(request, 'users/edit.html')
+        return render(request, 'users/edit_user.html')
+    
+def change_password(request, user_id):
+    if request.method == 'POST':
+        user = user_get_by_id(request, request.session['user_id'])
+        # TODO doesnt work, needs adjustments, needs to be hashed? 
+        if verify_password(user.password, request.POST['old_password']):
+            user.password = request.POST['password']
+            user.save()
+            return redirect('users:detail', user_id=user.id)
+        else:
+            return render(request, 'users/change_password.html', {'error': 'Špatné heslo'})
+    else:
+        return render(request, 'users/change_password.html')
